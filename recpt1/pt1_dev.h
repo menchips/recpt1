d33fb563b8fdc758e7c4fb3149ab214f276c3133
menchips <40588178+menchips@users.noreply.github.com>
2019-01-12 11:31:53 +0900

    デバイス名を非公式ドライバ名に準拠して修正

--- recpt1/pt1_dev.h
+++ recpt1/pt1_dev.h
@@ -15,10 +15,10 @@ char *bsdev[NUM_BSDEV] = {
     "/dev/pt3video0",
     "/dev/pt3video5",
     "/dev/pt3video4",
-    "/dev/pt4video0",
-    "/dev/pt4video1",
-    "/dev/pt4video4",
-    "/dev/pt4video5"
+    "/dev/px4video0",
+    "/dev/px4video1",
+    "/dev/px4video4",
+    "/dev/px4video5"
 };
 char *isdb_t_dev[NUM_ISDB_T_DEV] = {
     "/dev/pt1video2",
@@ -33,10 +33,10 @@ char *isdb_t_dev[NUM_ISDB_T_DEV] = {
     "/dev/pt3video3",
     "/dev/pt3video6",
     "/dev/pt3video7",
-    "/dev/pt4video2",
-    "/dev/pt4video3",
-    "/dev/pt4video6",
-    "/dev/pt4video7"
+    "/dev/px4video2",
+    "/dev/px4video3",
+    "/dev/px4video6",
+    "/dev/px4video7"
 };
 
 // 変換テーブル(ISDB-T用)
